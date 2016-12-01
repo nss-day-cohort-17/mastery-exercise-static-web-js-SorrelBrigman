90c5ee0cc5462abb51f0655bde61473490f8409e
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-01 14:40:28 -0600

    wrote out verbally the steps to solve function that builds tree

--- quiz.js
+++ quiz.js
@@ -1,5 +1,4 @@
-// Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
-var tree = function () {}
+
 // ### It accepts a single object as an argument. The object should have two key/value pairs.
 var myTree = {
   // 1. A key that specifies the height of the pine tree.
@@ -14,6 +13,19 @@ myTree.treeHeight = document.querySelector("how-tall").innerHTML;
 
 // 1. The character to use should be from user input in a `<input type="text">` field in the DOM.
 myTree.treeChar = document.querySelector("what-char").innerHTML
+
+
+// Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
+var tree = function () {
+  for (var i = 0; i <= myTree.treeHeight; i++) {
+    //include  3 apostrophes, then js
+    //number of spaces for top of tree = treeHeight - 1
+    //space decrease by one each line
+    //char increases by 2 each line
+    //base of tree = (treeHeight X 2) - 1
+    //conclude with three closing apostrophes
+
+  }
 // Once the user enters in a number, and a character,
    // the user can either then just press the enter key _(as long as the cursor is in one of the input fields)_,
    //or click a button that is labeled "Grow your tree" and the tree should be shown in the console.
@@ -21,3 +33,5 @@ myTree.treeChar = document.querySelector("what-char").innerHTML
 
 // If either of the input fields does not have a value in it when the user presses the enter key,
      //or presses the button, then display an alert stating that both fields must have a value.
+
+}
